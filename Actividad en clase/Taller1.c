3a0fbac58f686a2bc2afc011255f70b7f9793a9a
Ethan Flores <ethan.sebas.los@gmail.com>
2024-12-14 11:29:38 -0500

    Agregamos taller1

---
+++ Actividad en clase/Taller1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void main()
+{
+    int NrPersonas,edad,IMens=0,IWomans=0,contador = 0;
+    float PorcentajeH,PorcentajeM;
+    char genero;
+    printf("Ingrese la cantidad de personas:");
+    scanf("%d",&NrPersonas);
+    while (NrPersonas != contador)
+    {
+      printf("Ingrese la edad:");
+        scanf("%d",&edad);
+        printf("Ingrese el genero:");
+        scanf(" %c",&genero);
+        if(edad >= 18){
+            switch (genero)
+            {
+            case 'm':
+            case 'M':
+                IMens++;
+                break;
+            
+            case 'F':
+            case 'f':
+                IWomans++;
+                break;
+            }
+        }
+    contador++;  
+    }
+    
+    //calculo porcentaje
+    PorcentajeH = ((float)IMens / (float)NrPersonas)*100;
+    PorcentajeM = ((float)IWomans / (float)NrPersonas)*100;
+
+    printf("La cantidad de hombres mayores a 18 años es de %d y el porcentaje es de %.2f\n",IMens,PorcentajeH);
+    printf("La cantidad de mujeres mayores a 18 años es de %d y el porcentaje es de %.2f",IWomans,PorcentajeM);
+}
+
