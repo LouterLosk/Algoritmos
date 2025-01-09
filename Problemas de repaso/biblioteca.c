2d15ebb8c1c7e0073c03a97becfd1080f361d455
Ethan Flores <ethan.sebas.los@gmail.com>
2025-01-09 18:32:37 -0500

    Se realizo la funcion de numeros primos

--- Problemas de repaso/biblioteca.c
+++ Problemas de repaso/biblioteca.c
@@ -1,25 +1,45 @@
-#include<stdio.h>
+#include <stdio.h>
 
 
 
 /*Suma de números primos:Elabore un programa que le pida al usuario, a través del teclado, 
 un número positivo N menor que 100 y positivo. El sistema debe generar los números primos de 1 hasta N. 
 Además, deben desplegar la cantidad de numero primos que existe en ese rango. (4ptos)*/
-
-
-int primos(){
+int primos2(){
     int N;
-
+    int cont = 0,mod;
+    int i = 1;
+    int j = 1;
     do
     {
         printf("Ingrese un numero que sea menor a 100 y positivo: ");
         scanf("%d",&N);
     } while (N>100 || N < 0);
-    return 0;
-}   
+
+    for (i ; i <= N ; i++){
+        for (j ; j <= i ; j++){
+        mod = i % j;
+        if(mod == 0){
+            cont ++;
+        }
+        //printf("%d mod %d = %d conta %d ",i,j,mod,cont);
+        }
+        if (cont <= 2){
+                printf("o%d",i);
+
+            }
+        cont = 0;
+        j = 1;
+    }
+}
+
+
+
+
+
 
 int main()
 {
-    primos();
+    primos2();
     return 0;
 }
