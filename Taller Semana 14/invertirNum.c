04c9fc0759d70c42ccfd19b151e007f08a4003ca
Ethan Flores <ethan.sebas.los@gmail.com>
2025-01-09 20:53:56 -0500

    Se agrego los comentarios

--- Taller Semana 14/invertirNum.c
+++ Taller Semana 14/invertirNum.c
@@ -7,20 +7,26 @@ Entrada: 1234
 Salida: 4321*/
 
 int main(){
-    int Num,residuo,invertido,cont=0;
-    printf("Ingrese el numero que quiere voltear: ");
-    scanf("%d",&Num);
-    if(Num > 0){
+    int Num,residuo,invertido;
+    int cont= 0;//Variable que nos ayuda a contar
+    do//Este do repite el ingreso de numero hasta que sea valido
+    {
+        printf("Ingrese el numero que quiere voltear: ");
+        scanf("%d",&Num);//Ingreso del numero a voltear
+        //Validacion de que el numero es positivo
+        if(Num > 0){
         for (int i = 0; Num > 0; i++)
         {
+            //Se obtienen cada digito para invertirlo
             residuo = Num % 10;
             Num = Num / 10;
             invertido = invertido * 10;
             invertido = invertido + residuo;
         }
-        printf("El numero invertido es %d", invertido);
+        printf("El numero invertido es %d", invertido);//Salida del numero invertido
 
     }
     else
-    printf("El numero ingresado es invalido");
+    printf("El numero ingresado es invalido\n");     
+    } while (Num < 0);
 }
