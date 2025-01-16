7c10b12591c77315f1950d26bd2f9df42cafc75d
Ethan Flores <ethan.sebas.los@gmail.com>
2025-01-15 21:54:25 -0500

    Actualizacion boletos

--- Taller/BOLETOS.c
+++ Taller/BOLETOS.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
 /*
 1.	Solicita al usuario la distancia del viaje (en kilómetros) y calcula el costo del boleto con base en una tarifa por kilómetro. 
 2.	Pide el monto que paga el usuario y calcula el cambio. 
 3.	Devuelve el cambio en monedas de 5, 2 y 1 peso. 
 4.	Muestra un resumen con el costo del boleto, el monto pagado y el cambio entregado. 
 */
-
 int main()
 {
     int seguir;
-    const int Kilometro = 6;
+    const int Kilometro = 3;
     printf("Quiere calcular el precio a pagar? 1 = Si 0 = No\n");
     scanf("%d",&seguir);
 
@@ -35,17 +33,18 @@ int main()
         {
             printf("Con cuanto va a pagar?\n");
             scanf("%d",&ingreso);
-        } while (ingreso < 0);
+            if (ingreso < costo){
+                printf("El valor ingresado es menor al valor del viaje,faltan %d pesos\n",costo - ingreso);
+            }
+        } while (ingreso < 0 || ingreso < costo);
         printf("-------------------------------------------------\n");
         vuelto[0] = ingreso - costo;
         printf("Su vuelto es de %d pesos\n",vuelto[0]);
-        
         //Calculo de vuelto 1 2 5 pesos 
         vuelto[3] = vuelto[0] / 5;//Calculo de vuelto 5 pesos
         monedas = vuelto[0] % 5;
         vuelto[2] = monedas/2; //Calculo de vuelto 2 pesos
-        monedas = monedas%2;//Calculo de vuelto 1 pesos
-    
+        vuelto[1] = monedas%2;//Calculo de vuelto 1 pesos
         //Muestra un resumen con el costo del boleto, el monto pagado y el cambio entregado. 
         if (vuelto[3] > 0){
             printf("Con %.0f monedas de 5 pesos \n",(float)vuelto[3]);
@@ -60,6 +59,5 @@ int main()
         printf("Quiere calcular otro valor? 1 = Si 0 = No\n");
         scanf("%d",&seguir);
         }
-
  return 0;    
 }
